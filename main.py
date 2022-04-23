821a09d07e792b0f1ca1a3b46c7d0d6d559bd16e
Siva Veludurthi <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-23 09:49:49 +0000

    commit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# built in funtion and method search from websites
-greet =' HELLO'
-print(greet.lower())
-greet =' hello'
-print(greet.upper())
-greet =' hello helo then '
-print(greet.find('he'))
-print(greet.replace('hello','hi'))
+name = 'siva'
+is_cool =False
 
-
+is_cool =True
+print(bool,'True')
