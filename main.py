fde1e383a7d5bffe715c0c6e1440d1009c947e37
siva v <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-23 05:21:27 +0000

    math funtions

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-#power of 2 represent **
-print(2** 3)
-#reminder
-print(3//4)
-#Moduler
-print(5% 4)
-#6moduler 4
-print(6 %4)
+#math funtions,know more search python math funtions
+print(round(3.9))
 
 
 
