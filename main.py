b39315160912539f7c22a96bda4577a4e25bfaa6
Siva Veludurthi <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-23 06:40:56 +0000

    version of variable strings

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ age = 25
 print(f'hello' + 'my name is '+ name + 'my age is ' +str(age))
 name = 'siva'
 age = 25
+#python 3
 print(f'hello .my name is {name}. my age is {age}')
+#python 4
 print ('hi {}. You are {} years old'.format('siva','25'))
+print('hi{0}.you are {1} years old' .format('siva','25'))
