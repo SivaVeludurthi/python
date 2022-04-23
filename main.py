caf869bd6daba90ad9bae9161689a9cebeabe791
Siva Veludurthi <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-23 07:36:43 +0000

    string reversal

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-name = 'siva'
-age = 25
-print(f'hello' + 'my name is '+ name + 'my age is ' +str(age))
-name = 'siva'
-age = 25
-#python 3
-print(f'hello .my name is {name}. my age is {age}')
-#python 4
-print ('hi {}. You are {} years old'.format('siva','25'))
-print('hi{0}.you are {1} years old' .format('siva','25'))
+selfish ='012345678'
+print(selfish[0:2])
+print(selfish[2:7])
+print(selfish[::-1])
