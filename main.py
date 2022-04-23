de8108ebd691b3636df5b87eab2d406297d15361
Siva Veludurthi <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-23 06:10:48 +0000

    commmit

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-print(type('hellosiva'))
-username = 'supercoder'
-password = 'supersecet'
-long_String = '''
-
-wow 
-00
----
-'''
-print(long_String)
-fullname = username +' '+password
-print(fullname)
-#if use of space like this ' '
+print('hello' +'5')
+print('hello5')
