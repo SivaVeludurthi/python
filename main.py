63078f96b83e6f2f3a9e61a2174c4ffa7f95a7cd
Siva Veludurthi <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-23 09:46:13 +0000

    upper,lower,find,replace

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-selfish ='1235789'
-selfish ='2'
-print(selfish)
+# built in funtion and method search from websites
+greet =' HELLO'
+print(greet.lower())
+greet =' hello'
+print(greet.upper())
+greet =' hello helo then '
+print(greet.find('he'))
+print(greet.replace('hello','hi'))
+
+
