21128824162dce933239287dff321422a1f4a764
Siva Veludurthi <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-23 06:37:27 +0000

    logic behind

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ print(f'hello' + 'my name is '+ name + 'my age is ' +str(age))
 name = 'siva'
 age = 25
 print(f'hello .my name is {name}. my age is {age}')
+print ('hi {}. You are {} years old'.format('siva','25'))
