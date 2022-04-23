c4c017df41d3f047aa99fc6700cba974becee7c5
siva v <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-23 05:11:45 +0000

    int

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print('helloworld')
+#fundamental Data Types
+print(2 +4)
+print(2 -4)
+print(2 /4)
+print(2 *4)
+#let's take float and int 
+print(type(2+2.5))
+#Data Types
+print(type(2+3))
+print(type(2 -4))
+print(type(2/4))
+print(type(2*4))
+
+
+
 
