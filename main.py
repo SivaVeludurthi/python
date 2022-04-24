44a45d73524d7bb61a5e8c4d7cf83f29d9dcc245
Siva Veludurthi <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-24 10:32:23 +0000

    simple

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-#list slicing(should create new cart)
-amazon_Cart=[
-'notebooks', 
- 'milk',
- 'toys'
- ,'sunglasses']
-amazon_Cart[0] ='laptop'
-new_Cart = amazon_Cart[:]
-new_Cart[0]='gum'
-print(new_Cart)
-print(amazon_Cart)
+#Matrix
+matrix =[
+  [1,5,1],
+  [0,1,0],
+  [0,0,1]
+]
+print(matrix[0][1])
 
