7204ddab9b08f674957874d3974997687a32c17c
Siva Veludurthi <96187432+SivaVeludurthi@users.noreply.github.com>
2022-04-24 10:42:43 +0000

    remove

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 basket =[1,2,3,4,5,6]
 #adding 
-basket.insert(5,100)
 new_list = basket.extend([100])
+#removing
+basket.pop()
+basket.pop(0)
+basket.remove(6)
 print(basket)
-print(new_list)
+
 
 
